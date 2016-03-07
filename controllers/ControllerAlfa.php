45369916a3719c519ca4c43b288ac876b2acd1de
Titus Toia <titustoia@gmail.com>
2016-03-07 16:05:15 +0200

    first commit

---
+++ controllers/ControllerAlfa.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Etanova\controllers\portlets;
+use Etanova\Framework\controllers\portlets\BasePortlet;
+
+class PortletAlfa extends BasePortlet {
+
+	public function CreateModel() {
+	}
+
+}
